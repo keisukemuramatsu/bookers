1211c2948c3348e07084a6e516e4d595aad56598
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-07-14 10:17:02 +0000

    [Update]index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-    resources :books
+    get "/books" => "books#index"
+    post "/books" => "books#create"
+    get "/books/:id/edit" => "books#edit", as: "edit_books"
+    get "/books/:id" => "books#show", as:"book"
+    patch "/books/:id" => "books#update"
+    delete "/books/:id" => "books#destroy"
+    #resources :books で開発効率高まる
     get "/top" => "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
