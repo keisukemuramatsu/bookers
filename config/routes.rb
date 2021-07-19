e040f9a1ae16c85bb4853bbcc5fa2262e6ef9fd4
keisukemuramatsu <harukasoranoyouni@gmail.com>
2021-07-19 12:18:40 +0000

    [finish]3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-    get "/books" => "books#index"
-    post "/books" => "books#create"
-    get "/books/:id/edit" => "books#edit", as:"edit_books"
+    get "books" => "books#index"
+    post "books" => "books#create"
+    get "books/:id/edit" => "books#edit", as:"edit_book"
     #get "/books/test/edit" => "books#edit"
-    get "/books/:id" => "books#show", as:"book"
+    get "books/:id" => "books#show", as:"book"
     #get "/books/test" => "books#show"
-    patch "/books/:id" => "books#update"
-    delete "/books/:id" => "books#destroy"
+    patch "books/:id" => "books#update"
+    delete "books/:id" => "books#destroy"
     #resources :books で開発効率高まる
-    get "/" => "homes#top"
-    #root to: 'homes#top'←これでも"/"でトップ画面の表示となる
+    #get "/" => "homes#top"
+    root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
